33154edf3797a38849ff462244da953151907960
brightlego <12johnellis@gmail.com>
2024-03-30 13:26:40 +0000

    Added deck + tests

--- src/deck.rs
+++ src/deck.rs
@@ -1,7 +1,7 @@
 use std::cmp::Ordering;
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
-enum Suit {
+pub enum Suit {
     Clubs,
     Hearts,
     Diamonds,
@@ -9,7 +9,7 @@ enum Suit {
 }
 
 #[derive(Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Debug)]
-enum Value {
+pub enum Value {
     Two,
     Three,
     Four,
@@ -25,10 +25,28 @@ enum Value {
     Ace,
 }
 
-#[derive(PartialEq, Eq, Debug)]
-struct Card {
+const VALUES: [Value; 13] = [
+    Value::Two,
+    Value::Three,
+    Value::Four,
+    Value::Five,
+    Value::Six,
+    Value::Seven,
+    Value::Eight,
+    Value::Nine,
+    Value::Ten,
+    Value::Jack,
+    Value::Queen,
+    Value::King,
+    Value::Ace,
+];
+
+const SUITS: [Suit; 4] = [Suit::Clubs, Suit::Hearts, Suit::Diamonds, Suit::Spades];
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub struct Card {
     suit: Suit,
-    value: Value
+    value: Value,
 }
 
 impl PartialOrd for Card {
@@ -41,7 +59,89 @@ impl PartialOrd for Card {
     }
 }
 
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+pub struct Deck<T, const N: usize> {
+    cards: [T; N],
+    top: usize,
+}
 
+impl<T, const N: usize> From<[T; N]> for Deck<T, N> {
+    fn from(value: [T; N]) -> Self {
+        Deck {
+            cards: value,
+            top: 0,
+        }
+    }
+}
+
+impl<T, const N: usize> Deck<T, N> {
+    pub fn shuffle(&mut self) {
+        use rand::seq::SliceRandom;
+        let mut rng = rand::thread_rng();
+        let slice = &mut self.cards[self.top..];
+        slice.shuffle(&mut rng);
+    }
+
+    pub fn draw_one(&mut self) -> Option<&T> {
+        if self.top >= N {
+            None
+        } else {
+            let card = &self.cards[self.top];
+            self.top += 1;
+            Some(card)
+        }
+    }
+
+    pub fn draw_many(&mut self, n: usize) -> Vec<&T> {
+        let mut vec: Vec<&T> = vec![];
+        for _ in 0..n {
+            if let Some(card) = self.cards.get(self.top) {
+                vec.push(card);
+                self.top += 1;
+            } else {
+                return vec;
+            }
+        }
+        vec
+    }
+
+    pub fn get_one(&self) -> Option<&T> {
+        self.cards.get(self.top)
+    }
+
+    pub fn get_many(&self, n: usize) -> Vec<&T> {
+        let mut vec = Vec::with_capacity(n);
+        for card in &self.cards[self.top..std::cmp::min(self.top + n, N)] {
+            vec.push(card);
+        }
+        vec
+    }
+
+    pub fn reset(&mut self) {
+        self.top = 0;
+    }
+
+    pub fn get_all(&self) -> Vec<&T> {
+        self.get_many(N)
+    }
+}
+
+pub fn standard() -> Deck<Card, 52> {
+    let mut cards: [Card; 52] = [Card {
+        suit: Suit::Clubs,
+        value: Value::Ace,
+    }; 52];
+    for i in 0..SUITS.len() {
+        for j in 0..VALUES.len() {
+            cards[VALUES.len() * i + j] = Card {
+                suit: SUITS[i],
+                value: VALUES[j],
+            };
+        }
+    }
+    Deck::from(cards)
+}
+
+mod display;
 #[cfg(test)]
 mod tests;
-mod display;
--- src/deck/tests.rs
+++ src/deck/tests.rs
@@ -1,28 +1,5 @@
-use std::fmt::Debug;
 use super::*;
-
-const VALUES: [Value; 13] = [
-    Value::Two,
-    Value::Three,
-    Value::Four,
-    Value::Five,
-    Value::Six,
-    Value::Seven,
-    Value::Eight,
-    Value::Nine,
-    Value::Ten,
-    Value::Jack,
-    Value::Queen,
-    Value::King,
-    Value::Ace
-];
-
-const SUITS: [Suit; 4] = [
-    Suit::Clubs,
-    Suit::Hearts,
-    Suit::Diamonds,
-    Suit::Spades
-];
+use std::fmt::Debug;
 
 /// Tests whether a function applied to pairs of values in the slice return the same values
 /// as another function applied on the pairs of indices.
@@ -48,7 +25,12 @@ const SUITS: [Suit; 4] = [
 /// ```
 
 fn test_returns_same<T, V, F1, F2>(values: &[T], value_func: F1, index_func: F2)
-        where V: Eq, V: Debug, F1: Fn(&T, &T) -> V, F2: Fn(&usize, &usize) -> V {
+where
+    V: Eq,
+    V: Debug,
+    F1: Fn(&T, &T) -> V,
+    F2: Fn(&usize, &usize) -> V,
+{
     for i in 0..values.len() {
         for j in 0..values.len() {
             assert_eq!(value_func(&values[i], &values[j]), index_func(&i, &j));
@@ -77,8 +59,14 @@ fn card_ord() {
         for j in 0..VALUES.len() {
             for k in 0..SUITS.len() {
                 for l in 0..SUITS.len() {
-                    let card1 = Card { suit: SUITS[k], value: VALUES[i]};
-                    let card2 = Card { suit: SUITS[l], value: VALUES[j]};
+                    let card1 = Card {
+                        suit: SUITS[k],
+                        value: VALUES[i],
+                    };
+                    let card2 = Card {
+                        suit: SUITS[l],
+                        value: VALUES[j],
+                    };
                     let cmp = card1.partial_cmp(&card2);
 
                     if k == l {
@@ -90,4 +78,181 @@ fn card_ord() {
             }
         }
     }
+}
+
+fn equal_contents<T: PartialEq>(s1: &[T], s2: &[T]) -> bool {
+    if s1.len() != s2.len() {
+        return false;
+    }
+    for i in s1 {
+        if !s2.contains(i) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+#[test]
+fn deck_from() {
+    assert_eq!(Deck::from([]), Deck::<i32, 0> { cards: [], top: 0 });
+    assert_eq!(
+        Deck::from([1, 2, 3, 4, 5]),
+        Deck {
+            cards: [1, 2, 3, 4, 5],
+            top: 0
+        }
+    );
+}
+
+#[test]
+fn standard_deck() {
+    let deck = standard();
+    let mut expected = vec![];
+    for suit in SUITS {
+        for value in VALUES {
+            expected.push(Card { suit, value })
+        }
+    }
+    assert!(equal_contents(&deck.cards, &expected));
+}
+
+#[test]
+fn get_one() {
+    let deck = Deck::from([1, 2, 3]);
+    assert_eq!(deck.get_one(), Some(&1));
+
+    let deck = Deck {
+        cards: [1, 2, 3],
+        top: 1,
+    };
+    assert_eq!(deck.get_one(), Some(&2));
+
+    let deck = Deck {
+        cards: [1, 2, 3],
+        top: 3,
+    };
+    assert_eq!(deck.get_one(), None);
+
+    let deck: Deck<i32, 0> = Deck::from([]);
+    assert_eq!(deck.get_one(), None);
+}
+
+#[test]
+fn get_many() {
+    let deck = Deck::from([1, 2, 3]);
+    assert_eq!(deck.get_many(0), Vec::new() as Vec<&i32>);
+    assert_eq!(deck.get_many(1), vec![&1]);
+    assert_eq!(deck.get_many(2), vec![&1, &2]);
+    assert_eq!(deck.get_many(3), vec![&1, &2, &3]);
+    assert_eq!(deck.get_many(4), vec![&1, &2, &3]);
+
+    let deck = Deck {
+        cards: [1, 2, 3],
+        top: 1,
+    };
+
+    assert_eq!(deck.get_many(0), Vec::new() as Vec<&i32>);
+    assert_eq!(deck.get_many(1), vec![&2]);
+    assert_eq!(deck.get_many(2), vec![&2, &3]);
+    assert_eq!(deck.get_many(3), vec![&2, &3]);
+
+    let deck = Deck {
+        cards: [1, 2, 3],
+        top: 3,
+    };
+    assert_eq!(deck.get_many(1), Vec::new() as Vec<&i32>);
+
+    let deck: Deck<i32, 0> = Deck::from([]);
+    assert_eq!(deck.get_many(1), Vec::new() as Vec<&i32>);
+}
+
+#[test]
+fn get_all() {
+    let deck = Deck::from([1, 2, 3]);
+    assert_eq!(deck.get_all(), vec![&1, &2, &3]);
+
+    let deck = Deck {
+        cards: [1, 2, 3],
+        top: 1,
+    };
+    assert_eq!(deck.get_all(), vec![&2, &3]);
+
+    let deck = Deck {
+        cards: [1, 2, 3],
+        top: 3,
+    };
+    assert_eq!(deck.get_all(), Vec::new() as Vec<&i32>);
+
+    let deck: Deck<i32, 0> = Deck::from([]);
+    assert_eq!(deck.get_all(), Vec::new() as Vec<&i32>);
+}
+
+#[test]
+fn draw_one() {
+    let mut deck = Deck::from([1, 2, 3]);
+    assert_eq!(deck.draw_one(), Some(&1));
+    assert_eq!(deck.draw_one(), Some(&2));
+    assert_eq!(deck.draw_one(), Some(&3));
+    assert_eq!(deck.draw_one(), None);
+
+    let mut deck: Deck<i32, 0> = Deck::from([]);
+    assert_eq!(deck.draw_one(), None);
+    assert_eq!(deck.draw_one(), None);
+}
+
+#[test]
+fn draw_many() {
+    let mut deck = Deck::from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    assert_eq!(deck.draw_many(0), Vec::new() as Vec<&i32>);
+    assert_eq!(deck.draw_many(1), vec![&1]);
+    assert_eq!(deck.draw_many(1), vec![&2]);
+    assert_eq!(deck.draw_many(0), Vec::new() as Vec<&i32>);
+    assert_eq!(deck.draw_many(3), vec![&3, &4, &5]);
+    assert_eq!(deck.draw_many(10), vec![&6, &7, &8, &9, &10]);
+    assert_eq!(deck.draw_many(10), Vec::new() as Vec<&i32>);
+}
+
+#[test]
+fn shuffle() {
+    // A large deck so there is a 1 in ~10^68 chance of shuffling to the starting position
+    let orig = standard();
+    let mut deck = orig.clone();
+    deck.shuffle();
+    for _ in 0..10 {
+        assert_ne!(orig, deck);
+        assert_eq!(orig.top, deck.top);
+    }
+
+    let orig = Deck {
+        cards: orig.cards,
+        top: 20,
+    };
+    let mut deck = orig.clone();
+
+    for _ in 0..10 {
+        deck.shuffle();
+
+        assert_eq!(orig.cards[..20], deck.cards[..20]);
+        assert_ne!(orig.cards[20..], deck.cards[20..]);
+        assert_eq!(orig.top, deck.top);
+    }
+
+    let orig = Deck {
+        cards: orig.cards,
+        top: 52,
+    };
+    let mut deck = orig.clone();
+    deck.shuffle();
+    assert_eq!(orig, deck)
+}
+
+#[test]
+fn reset() {
+    let orig = standard();
+    let mut deck = orig.clone();
+    let _ = deck.draw_many(20);
+    assert_ne!(deck, orig);
+    deck.reset();
+    assert_eq!(deck, orig);
 }
